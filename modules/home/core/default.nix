e76caa2c7c5b4477ae5f6e1eb9bae506fc90109c
xhos <xxhos@pm.me>
2024-07-23 13:56:24 +0900

    some styling refactoring

--- modules/home/core/default.nix
+++ modules/home/core/default.nix
@@ -1,9 +1,25 @@
 { inputs, config, pkgs, ... }: {
   wallpaper = ../../../home/shared/walls/${config.theme}.jpg;
-  background = config.lib.stylix.colors.base00;
-  text = config.lib.stylix.colors.base05;
+  
   accent = config.lib.stylix.colors.base0D;
   
+  background = config.lib.stylix.colors.base00; 
+  mantle     = config.lib.stylix.colors.base01; 
+  surface0   = config.lib.stylix.colors.base02; 
+  surface1   = config.lib.stylix.colors.base03; 
+  surface2   = config.lib.stylix.colors.base04; 
+  text       = config.lib.stylix.colors.base05; 
+  rosewater  = config.lib.stylix.colors.base06; 
+  lavender   = config.lib.stylix.colors.base07; 
+  red        = config.lib.stylix.colors.base08; 
+  peach      = config.lib.stylix.colors.base09; 
+  yellow     = config.lib.stylix.colors.base0A; 
+  green      = config.lib.stylix.colors.base0B; 
+  teal       = config.lib.stylix.colors.base0C; 
+  blue       = config.lib.stylix.colors.base0D; 
+  mauve      = config.lib.stylix.colors.base0E; 
+  flamingo   = config.lib.stylix.colors.base0F; 
+
   home.sessionVariables = rec {
     ACCENT = config.accent;
     BACKGROUND = config.background;
--- modules/home/core/options.nix
+++ modules/home/core/options.nix
@@ -20,4 +20,60 @@ with lib; {
     type = types.str;
     default = "";
   };
+  options.mantle = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.surface0 = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.surface1 = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.surface2 = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.rosewater = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.lavender = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.red = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.peach = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.yellow = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.green = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.teal = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.blue = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.mauve = mkOption {
+    type = types.str;
+    default = "";
+  };
+  options.flamingo = mkOption {
+    type = types.str;
+    default = "";
+  };
 }
