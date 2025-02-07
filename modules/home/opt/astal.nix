7529dde209940eb768a6bce84610b52e0894c017
xhos <60789741+xhos@users.noreply.github.com>
2025-02-07 10:58:41 -0500

    astal tweaks

--- modules/home/opt/astal.nix
+++ modules/home/opt/astal.nix
@@ -9,5 +9,9 @@
     home.packages = with pkgs; [
       inputs.astal-shell.packages.${system}.default
     ];
+
+    wayland.windowManager.hyprland.settings = {
+      exec = ["my-shell"];
+    };
   };
 }
