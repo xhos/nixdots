f7edc429d67132a75815fcc248d785a8641c93f7
xhos <60789741+xhos@users.noreply.github.com>
2025-02-27 19:04:23 -0500

    steam tweaks

--- modules/nixos/opt/steam.nix
+++ modules/nixos/opt/steam.nix
@@ -5,20 +5,53 @@
   ...
 }: {
   config = lib.mkIf config.steam.enable {
+    environment.systemPackages = with pkgs; [gamescope];
+    nixpkgs.config.packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+        extraPkgs = pkgs:
+          with pkgs; [
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+            protonup
+          ];
+      };
+    };
+
     programs = {
       steam = {
         enable = true;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        # gamescopeSession.enable = true;
+        gamescopeSession.enable = true;
+        package = pkgs.steam.override {
+          extraPkgs = pkgs:
+            with pkgs; [
+              libkrb5
+              keyutils
+            ];
+        };
       };
       gamemode.enable = true;
     };
 
     environment.sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/xhos/.steam/root/.compatibilitytools.d";
+      GDK_BACKEND = "wayland,x11";
+      QT_QPA_PLATFORM = "wayland;xcb";
+      #SDL_VIDEODRIVER = "x11";
+      CLUTTER_BACKEND = "wayland";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      WLR_NO_HARDWARE_CURSORS = "1";
     };
-
-    environment.systemPackages = with pkgs; [protonup];
   };
 }
