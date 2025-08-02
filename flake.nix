193713e5121511315ba3fc8114fae049b99583e2
Mark <60789741+xhos@users.noreply.github.com>
2025-08-02 18:22:14 +0000

    fix typo

--- flake.nix
+++ flake.nix
@@ -50,110 +50,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # priavte secrets repository
-    nix-secrets = {
-      url = "git+ssh://git@github.com/xhos/nix-secrets?shallow=1&ref=main";
-      flake = false;
-    };
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nixos-wsl,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    # Or 'nh os switch'
-    nixosConfigurations = {
-      zireael = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          home-manager.nixosModules.home-manager
-          inputs.stylix.nixosModules.stylix
-          {
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              system = "x86_64-linux";
-            };
-            home-manager.users."xhos" = ./home/xhos/zireael.nix;
-          }
-          ./hosts/zireael/configuration.nix
-        ];
-      };
-      vyverne = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              system = "x86_64-linux";
-            };
-            home-manager.users."xhos" = ./home/xhos/vyverne.nix;
-          }
-          inputs.stylix.nixosModules.stylix
-          inputs.disko.nixosModules.disko
-          inputs.impermanence.nixosModules.impermanence
-          inputs.sops-nix.nixosModules.sops
-          ./hosts/vyverne/configuration.nix
-        ];
-      };
-      aevon = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        system = "x86_64-linux";
-        modules = [
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              system = "x86_64-linux";
-            };
-            home-manager.users."xhos" = ./home/xhos/aevon.nix;
-          }
-          inputs.stylix.nixosModules.stylix
-          ./hosts/aevon/configuration.nix
-          nixos-wsl.nixosModules.default
-        ];
-      };
-      # nix run nixpkgs#nixos-generators -- --format iso --flake .#nyx -o iso
-      nyx = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/nyx/configuration.nix
-        ];
-      };
-    };
-    {
-  description = "overcomplicated mess of a flake that works for some reason";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-24.11";
-    impermanence.url = "github:nix-community/impermanence";
-    disko.url = "github:nix-community/disko";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    nixcord.url = "github:kaylorben/nixcord";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    aard.url = "github:xhos/aard";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # private secrets repository
     nix-secrets = {
       url = "git+ssh://git@github.com/xhos/nix-secrets?shallow=1&ref=main";
       flake = false;
@@ -164,7 +61,11 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-  in {
+  in
+  {
+    # ------------------------------
+    # NixOS configuration entrypoint
+    # ------------------------------
     nixosConfigurations = {
       zireael = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
@@ -210,19 +111,21 @@
         ];
       };
 
+      # nix run nixpkgs#nixos-generators -- --format iso --flake .#nyx -o iso
       nyx = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
-        modules = [ ./hosts/nyx/configuration.nix ];
+        modules = [
+          ./hosts/nyx/configuration.nix
+        ];
       };
     };
 
-    ########################
+    # ------------------------------
     # nix run entrypoint(s)
-    ########################
+    # ------------------------------
     apps.${system}.aevon-install = let
       script = pkgs.writeShellApplication {
         name = "aevon-install";
-        # tools the script depends on at runtime
         runtimeInputs = [ pkgs.git pkgs.coreutils pkgs.util-linux ];
         text = ''
           #!/usr/bin/env bash
@@ -281,40 +184,36 @@ EOM
       program = "${script}/bin/aevon-install";
     };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    # Or 'nh home switch'
+    # ------------------------------
+    # Standalone home-manager configs
+    # ------------------------------
     homeConfigurations = {
       "xhos@zireael" = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {
-          inherit inputs;
-          system = "x86_64-linux";
-        };
+        extraSpecialArgs = { inherit inputs; system = "x86_64-linux"; };
         modules = [
           ./home/xhos/zireael.nix
           inputs.sops-nix.homeManagerModules.sops
           inputs.stylix.homeModules.stylix
         ];
       };
+
       "xhos@vyverne" = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            inputs.aard.overlay
-          ];
+          overlays = [ inputs.aard.overlay ];
         };
-        # pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs system;};
+        extraSpecialArgs = { inherit inputs system; };
         modules = [
           ./home/xhos/vyverne.nix
           inputs.sops-nix.homeManagerModules.sops
           inputs.stylix.homeModules.stylix
         ];
       };
+
       "xhos@aevon" = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs;};
+        extraSpecialArgs = { inherit inputs; };
         modules = [
           ./home/xhos/aevon.nix
           inputs.stylix.homeModules.stylix
