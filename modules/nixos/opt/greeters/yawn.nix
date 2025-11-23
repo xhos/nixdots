4d28415ad5c08d7645d59ade0bcfc0cda79e9417
xhos <60789741+xhos@users.noreply.github.com>
2025-11-23 12:30:12 -0500

    bump yawn

--- modules/nixos/opt/greeters/yawn.nix
+++ modules/nixos/opt/greeters/yawn.nix
@@ -7,7 +7,7 @@
   config = lib.mkIf (config.greeter == "yawn") {
     services.greetd = {
       enable = true;
-      settings.default_session.command = "${inputs.yawn.packages.x86_64-linux.default}/bin/yawn -cmd Hyprland -user xhos";
+      settings.default_session.command = "${inputs.yawn.packages.x86_64-linux.default}/bin/yawn -cmd Hyprland -user xhos -preauth";
     };
   };
 }
