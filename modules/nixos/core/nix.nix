09397bda4fee71bf8c1f8456cb4e90379def84dc
xhos <60789741+xhos@users.noreply.github.com>
2025-12-16 19:39:58 -0500

    move uname fix from games to nix

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -10,6 +10,7 @@
         "https://nix-community.cachix.org"
         "https://ghostty.cachix.org"
         "https://cache.saumon.network/proxmox-nixos"
+        "https://cache.garnix.io"
       ];
 
       trusted-public-keys = [
@@ -19,6 +20,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
         "proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
 
       experimental-features = ["nix-command" "flakes"];
@@ -34,6 +36,27 @@
 
     optimise.automatic = true;
   };
+
+  # nix, esspecially with impermanence tends to open
+  # a lot of files resulting in "too many open files"
+  # errors. this fixes it. also, i have no idea which
+  # one actually works :D
+  boot.kernel.sysctl."fs.file-max" = 524288;
+  systemd.user.extraConfig = "DefaultLimitNOFILE=524288";
+  security.pam.loginLimits = [
+    {
+      domain = "*";
+      type = "soft";
+      item = "nofile";
+      value = "65536";
+    }
+    {
+      domain = "*";
+      type = "hard";
+      item = "nofile";
+      value = "524288";
+    }
+  ];
 
   system.stateVersion = "25.05";
 }
--- modules/nixos/opt/games.nix
+++ modules/nixos/opt/games.nix
@@ -10,27 +10,5 @@
     ];
 
     programs.steam.enable = true;
-
-    # GTNH opens *way too many* files
-    # and theese are 3 ways to increase the limit
-    # and i have no idea which one actually works :D
-    boot.kernel.sysctl."fs.file-max" = 524288;
-    systemd.user.extraConfig = ''
-      DefaultLimitNOFILE=524288
-    '';
-    security.pam.loginLimits = [
-      {
-        domain = "*";
-        type = "soft";
-        item = "nofile";
-        value = "65536";
-      }
-      {
-        domain = "*";
-        type = "hard";
-        item = "nofile";
-        value = "524288";
-      }
-    ];
   };
 }
